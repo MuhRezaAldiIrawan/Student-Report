8e9d611b1e8d25016f35fd1cd1145628c0cec56d
MuhRezaAldiIrawan <titaniaelvs@gmail.com>
2024-03-21 10:59:18 +0800

    add dosen data

--- app/Http/Controllers/MasterData/DosenController.php
+++ app/Http/Controllers/MasterData/DosenController.php
@@ -4,27 +4,132 @@ namespace App\Http\Controllers\MasterData;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
 use App\Models\User;
-use Datatables;
+use RealRashid\SweetAlert\Facades\Alert;
+use Illuminate\Support\Facades\DB;
+
 
 class DosenController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
     public function index(Request $request)
     {
+        $title = 'Dosen Data Page';
+
         if ($request->ajax()) {
-            $data = User::select('*');
+            $data = User::where('role', 'dosen')->latest()->get();
             return Datatables::of($data)
                     ->addIndexColumn()
                     ->addColumn('action', function($row){
-
-                            $btn = '<a href="javascript:void(0)" class="edit btn btn-primary btn-sm">View</a>';
-
+                            $btn =
+                            '<button class="btn btn-icon btn-success" data-toggle="tooltip" data-placement="top" title="detail">
+                                <i class="fas fa-eye"></i>
+                            </button>
+                            <button class="btn btn-icon btn-primary" data-toggle="tooltip" data-placement="top" title="edit">
+                                <i class="far fa-edit"></i>
+                            </button>
+                            <button class="btn btn-icon btn-danger"  data-toggle="tooltip" data-placement="top" title="edit">
+                                <i class="far fa-trash-alt"></i>
+                            </button>';
                             return $btn;
                     })
                     ->rawColumns(['action'])
                     ->make(true);
         }
+        return view('pages.dosen.index', compact('title'));
+    }
 
-        return view('pages.dosen.index');
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required|min:3',
+            'address' => 'required',
+            'phone' => 'required',
+            'role' => 'required',
+            'gender' => 'required',
+            'avatar' => 'image|file|mimes:jpeg,png,jpg,svg',
+
+        ]);
+
+        if ($request->file('avatar')) {
+            $validatedData['avatar'] = $request->file('avatar')->store('users-avatar');
+        }
+
+        User::create($validatedData);
+
+        Alert::success('Success', 'Dosen berhasil ditambahkan');
+
+        return redirect('/dosen');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+
+        dd($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        DB::table('users')->where('id', $id)->delete();
+        Alert::success('Success', 'Data Dosen berhasil dihapus');
+        return redirect('/dosen');
     }
 }
--- resources/views/pages/dosen/index.blade.php
+++ resources/views/pages/dosen/index.blade.php
@@ -2,40 +2,50 @@
 
 @section('content')
 
-
-
-<div class="table-responsive">
-    <table class="table table-hover data-table" id="data-table">
-        <thead>
-            <tr>
-                <th scope="col">No</th>
-                <th scope="col">Nama</th>
-                <th scope="col">Email</th>
-                <th scope="col">No HP</th>
-                <th scope="col">Alamat</th>
-                <th scope="col">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-
-        </tbody>
-    </table>
+<div class="card">
+    <div class="card-body">
+        <div class="d-flex align-items-center justify-content-between">
+            <h4>Dosen</h4>
+            <button class="btn btn-icon btn-primary btn-tone" data-toggle="modal" data-target=".bd-example-modal-xl">
+                <i class="fas fa-user-plus"></i>
+            </button>
+        </div>
+        <div class="m-t-25">
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover data-table">
+                    <thead>
+                        <tr>
+                            <th scope="col" style="text-align: center">No</th>
+                            <th scope="col" style="text-align: center">Nama</th>
+                            <th scope="col" style="text-align: center">Email</th>
+                            <th scope="col" style="text-align: center">No HP</th>
+                            <th scope="col" style="text-align: center">Alamat</th>
+                            <th scope="col" style="width: 15%; text-align: center">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-center">
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
-
 
 @endsection
 
+@component('components.modal-add-dosen')@endcomponent
+
 @push('js')
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.11.4/css/dataTables.bootstrap5.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
     <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> --}}
 
     <!-- page js -->
-    {{-- <script src="{{asset('vendors/datatables/jquery.dataTables.min.js')}}"></script>
-    <script src="{{asset('vendors/datatables/dataTables.bootstrap.min.js')}}"></script> --}}
+    <script src="{{asset('vendors/datatables/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('vendors/datatables/dataTables.bootstrap.min.js')}}"></script>
 
     <script type="text/javascript">
         $(function () {
@@ -45,7 +55,7 @@
             serverSide: true,
             ajax: "{{ route('dosen') }}",
             columns: [
-                {data: 'id', name: 'id'},
+                {data: 'DT_RowIndex', name: 'DT_RowIndex', },
                 {data: 'name', name: 'name'},
                 {data: 'email', name: 'email'},
                 {data: 'phone', name: 'phone'},
@@ -61,5 +71,5 @@
 
 @push('css')
     <!-- page css -->
-    {{-- <link href="{{asset('vendors/datatables/dataTables.bootstrap.min.css')}}" rel="stylesheet"> --}}
+    <link href="{{asset('vendors/datatables/dataTables.bootstrap.min.css')}}" rel="stylesheet">
 @endpush
