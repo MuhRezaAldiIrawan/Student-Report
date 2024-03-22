cfae436162bb113ed8622b0317ed895a75339f26
MuhRezaAldiIrawan <titaniaelvs@gmail.com>
2024-03-22 15:59:29 +0800

    wip

--- resources/views/pages/dosen/edit.blade.php
+++ resources/views/pages/dosen/edit.blade.php
@@ -1,6 +1,7 @@
 <div class="modal fade bd-example-modal-edit" id="editmodal">
     <div class="modal-dialog modal-xl">
         <div class="modal-content">
+
             <div class="modal-header">
                 <h5 class="modal-title h4">Edit Dosen</h5>
                 <button type="button" class="close" data-dismiss="modal">
